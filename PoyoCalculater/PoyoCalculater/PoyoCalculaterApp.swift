e15da4b7a4341a1550ffab6064c1a438ca418a9b
NiraiHayakawa <91765262+NiraiHayakawa@users.noreply.github.com>
2022-12-04 03:44:47 -0800

    Initial commit

---
+++ PoyoCalculater/PoyoCalculater/PoyoCalculaterApp.swift
@@ -0,0 +1,18 @@
+//
+//  PoyoCalculaterApp.swift
+//  PoyoCalculater
+//
+//  Created by geo on 2022/12/04.
+//
+//
+
+import SwiftUI
+
+@main
+struct PoyoCalculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
